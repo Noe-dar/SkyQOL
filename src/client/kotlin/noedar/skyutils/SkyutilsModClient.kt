2b697f67b6f9ecebacdd4eabe453bf9813026bea
Raddeonn <radeonchik.new@gmail.com>
2023-07-10 21:07:20 +0300

    Initial commit

---
+++ src/client/kotlin/noedar/skyutils/SkyutilsModClient.kt
@@ -0,0 +1,9 @@
+package noedar.skyutils
+
+import net.fabricmc.api.ClientModInitializer
+
+object SkyutilsModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
